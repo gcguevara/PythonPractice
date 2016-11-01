acba4831e0f598507e354bbb2c9dcb1fc76a127c
gcguevara <garrettcguevara@gmail.com>
2016-11-01 16:55:31 -0700

    Add files via upload

---
+++ ta_range_drill.py
@@ -0,0 +1,20 @@
+''' GARRETT GUEVARA
+    TECH ACADEMY
+    PYTHON, ITEM 43 DRILL
+'''
+
+# use a range function to display 0, 1, 2, 3
+print("Number one: ")
+for i in range(4):
+    print(i)
+
+# use a range function with 3 parameters to display 3, 2, 1, 0
+print("Number two: ")
+for i in range (3, -1, -1):
+    print(i)
+
+# use a range function with 3 parameters to display 8, 6, 4, 2
+print("Number three: ")
+for i in range (8, 0, -2):
+    print(i)
+ 
